0fd8c040c2941d2a36060b25ac45b4489154b358
hametvega <hamet2303@gmail.com>
2024-02-28 09:44:38 -0500

    Directorio 2

--- App/App.py
+++ App/App.py
@@ -1,9 +1,19 @@
 #los templates son las vistas o pestañas de
 
 from flask import * #para importar las cosas de una libreria
+import mysql.connector #importamos el SQL
+
+#configurar conexción
+db =mysql.connector.connect(
+    host="localhost", #valor por default
+    user="root",
+    pasword="",
+    database="agenda2024"
+)
+cursor =db.cursor() #es la connxión 
+
 
 #Crear una instancia de la clase flsk
-
 
 #Definir rutas
 app=Flask(__name__)
@@ -16,9 +26,26 @@ def index():
 def Saludo():
     return"Creando rutas al pryoecto"
 
-@app.route('/Registrar')
+@app.route('/Registrar', methods=['POST'])
 def Registrar():
-    return render_template('Registrar.html')
+
+    Nombres =request.form['name'],
+    Apellidos= request.form['apellido']
+    usuario = request.form['usuario']
+    correo = request.form['correo']
+    Celular = request.form['celular']
+    dirección= request.form['direccion']
+    contraseña= request.form['contrasena']
+    
+    #insertar datos en la tabla
+    cursor.execute("INSERT INTO Personas (Nombre_persona, Apellido_persona, Nombre_usuario, correo, celular, dirección, contraseña) VALUES(%s, %s, %s, %s, %s, %s, %s,)",(Nombres, Apellidos, usuario, correo, Celular, dirección, contraseña,) )
+    db.commit()
+    
+    return redirect(url_for('Registrar'))
+
+
+
+
 
 
 #Se ejecuta la app o el servidor
